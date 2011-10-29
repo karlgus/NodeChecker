e547e60f926c04675aa967fb48f3a8c2041f6bd8
Karl Gustafsson <karlgus@gmail.com>
2011-10-29 17:12:49 +0200

    Added classes to the program Added class nodes and nodes_result to the program

--- main.cpp
+++ main.cpp
@@ -1,10 +1,61 @@
+
 #include <iostream>
+#include <pugixml.hpp>
+#include <vector>
 
 using namespace std;
 
+class nodes
+{
+public:
+    vector <string> ip;
+    vector <string> hostname;
+};
+
+class nodes_result : nodes
+{
+public:
+    long int date;
+    long int time;
+    vector <string> ip;
+    vector <string> hostname;
+    vector <string> ping_max;
+    vector <string> ping_avg;
+    vector <string> ping_min;
+};
+
+
 int main()
 {
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file("nodes.xml");
 
-    cout << "hej" << endl;
+    if (result)
+    {
+
+        pugi::xpath_node_set tools = doc.select_nodes("/nodes/node/");
+
+        for(pugi::xpath_node_set::const_iterator it = tools.begin(); it != tools.end(); ++it)
+        {
+            pugi::xpath_node node = *it;
+
+            //test.text = node.node().child_value("ip");
+
+            //test.correctanswer = node.node().child("hostname").attribute("correctanswer").as_int();
+
+            //pugi::xpath_node_set tools2 = node.node().select_nodes("options/option");
+
+            //for(pugi::xpath_node_set::const_iterator it2 = tools2.begin(); it2 != tools2.end(); ++it2)
+            //{
+            //    pugi::xpath_node node = *it2;
+            //    test.optionsnr.push_back(node.node().child_value());
+            //}
+
+            //quest.push_back(test);
+            //test.optionsnr.clear();
+       }
+
+   }
+
     return 0;
 }
