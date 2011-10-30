7c982baf936e0090b335c76695e8207229ec7bd0
Karl Gustafsson <karlgus@gmail.com>
2011-10-30 10:20:43 +0100

    Kan nu läsa xml-filen nodes.xml Fick ändra till hårdkodad sökväg till xml-filen då den inte kunde läsa den.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-
 #include <iostream>
 #include <pugixml.hpp>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
 
@@ -28,40 +28,23 @@ public:
 int main()
 {
     pugi::xml_document doc;
-    pugi::xml_parse_result result = doc.load_file("nodes.xml");
+    pugi::xml_parse_result result = doc.load_file("/home/karl/.config/NodeChecker/nodes.xml");
 
 
     vector <Node> nodes;
 
-
     if (result)
     {
-
-        pugi::xpath_node_set tools = doc.select_nodes("/nodes/node/");
+       // pugi::xpath_node_set tools = doc.select_nodes("/nodes/node");
+        pugi::xpath_node_set tools = doc.select_nodes("/nodes/node");
 
         for(pugi::xpath_node_set::const_iterator it = tools.begin(); it != tools.end(); ++it)
         {
             Node tempnodes;
             pugi::xpath_node node = *it;
             tempnodes.ip = node.node().child_value("ip");
+            tempnodes.hostname = node.node().child_value("hostname");
             nodes.push_back(tempnodes);
-            //Nodes.ip = node.node().child_value("ip");
-            //Node.push_back(Nodes);
-            //nodes.ip = node.node().child_value("ip");
-            //test.text = node.node().child_value("ip");
-
-            //test.correctanswer = node.node().child("hostname").attribute("correctanswer").as_int();
-
-            //pugi::xpath_node_set tools2 = node.node().select_nodes("options/option");
-
-            //for(pugi::xpath_node_set::const_iterator it2 = tools2.begin(); it2 != tools2.end(); ++it2)
-            //{
-            //    pugi::xpath_node node = *it2;
-            //    test.optionsnr.push_back(node.node().child_value());
-            //}
-
-            //quest.push_back(test);
-            //test.optionsnr.clear();
        }
 
    }
